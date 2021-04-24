8fec95248f12ebcf7960b13d44d5f21bb7edc907
Mitch Lui <katinlui@gmail.com>
2021-04-24 21:16:30 +0100

    Confidence level min 80%

--- application.py
+++ application.py
@@ -2,13 +2,16 @@ from mask_detector import MaskDetector
 from train_mask_detector import ModelTrainer
 from arduino_adapter import ArduinoAdapter
 from loguru import logger
-from threading import Thread
+from imutils.video import VideoStream
+from time import sleep
 import argparse
+import imutils
+import cv2
 import os
 
 class Application:
 
-    def __init__(self, model: str, retrain_model: bool = False, dataset: str = "", face_detector: str = "", confidence: int = 0.5, source: int = 0) -> None:
+    def __init__(self, model: str, retrain_model: bool = False, dataset: str = "", face_detector: str = "", confidence: int = 0.8, source: int = 0) -> None:
         if retrain_model or not os.path.isfile(model):
             logger.info("Retraining model...")
             self.retrain_model(dataset, model)
@@ -21,9 +24,60 @@ class Application:
         model_trainer.train()
 
     def run(self):
-        #TODO Thread run
-        self.mask_detector.run()
+        logger.info("starting video stream...")
+        vs = VideoStream(src=self.mask_detector.source).start()
+        sleep(2)
+        while True:
+            # grab the frame from the threaded video stream and resize it
+            # to have a maximum width of 400 pixels
+            
+            mask_array = []
 
+            frame = vs.read()
+            frame = imutils.resize(frame, width=400)
+            locs,preds = self.mask_detector.detect_and_predict_mask(frame)
+            # detect faces in the frame and determine if they are wearing a
+            # face mask or not
+            
+            # loop over the detected face locations and their corresponding locations
+            for (box, pred) in zip(locs, preds):
+                # unpack the bounding box and predictions
+                (startX, startY, endX, endY) = box
+                (mask, withoutMask) = pred
+
+                # determine the class label and color we'll use to draw the bounding box and text
+                label = "Mask" if mask > withoutMask else "No Mask"
+                color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+
+                # include the probability in the label
+                label = f"{label}: {round(max(mask, withoutMask) * 100, 2)}%"
+
+                # display the label and bounding box rectangle on the output frame
+                cv2.putText(frame, label, (startX, startY - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+                cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+
+                if mask >= withoutMask:
+                    mask_array.append(True)
+                else:
+                    mask_array.append(False)
+
+            # show the output frame
+            cv2.imshow("Mask Detector Prototype", frame)
+            key = cv2.waitKey(1) & 0xFF
+
+            if not any(mask_array):
+                #TODO LIGHT UP LED
+                logger.warning(f"Someone is not wearing a mask.")
+            else:
+                logger.debug(f"Everyone is wearing a mask.")
+                
+
+            # if the `q` key was pressed, break from the loop
+            if key == ord("q"):
+                break
+        cv2.destroyAllWindows()
+        vs.stop()
 
 
 def run(): 
@@ -32,7 +86,7 @@ def run():
     ap.add_argument("-m", "--model", type=str, default="mask_detector.model", help="path to output face mask detector model")
     ap.add_argument("-r", "--retrain", action="store_true", help="path to input dataset")
     ap.add_argument("-d", "--dataset", default="dataset", help="path to input dataset")
-    ap.add_argument("-c", "--confidence", type=float, default=0.5, help="minimum probability to filter weak detections")
+    ap.add_argument("-c", "--confidence", type=float, default=0.8, help="minimum probability to filter weak detections")
     ap.add_argument("-s", "--source", type=int, default=0, help="integer for source camera")
     args = vars(ap.parse_args())
     app = Application(args["model"], args["retrain"], args["dataset"], args["face"], args["confidence"], args["source"])
--- mask_detector.py
+++ mask_detector.py
@@ -1,11 +1,8 @@
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
-from imutils.video import VideoStream
+from loguru import logger
 import numpy as np
-import argparse
-import imutils
-import time
 import cv2
 import os
 
@@ -16,10 +13,11 @@ class MaskDetector():
 		self.model = model
 		self.confidence = confidence
 		self.source = source
+		logger.info("loading face detector model...")
 		self.prototxtPath = os.path.sep.join([self.face_detector, "deploy.prototxt"])
-		self.weightsPath = os.path.sep.join([self.face_detector,
-		"res10_300x300_ssd_iter_140000.caffemodel"])
+		self.weightsPath = os.path.sep.join([self.face_detector, "res10_300x300_ssd_iter_140000.caffemodel"])
 		self.faceNet = cv2.dnn.readNet(self.prototxtPath, self.weightsPath)
+		logger.info("loading face mask detector model...")
 		self.maskNet = load_model(self.model)
 
 	def detect_and_predict_mask(self,frame):
@@ -76,70 +74,3 @@ class MaskDetector():
 			faces = np.array(faces, dtype="float32")
 			preds = self.maskNet.predict(faces, batch_size=32)
 		return locs,preds
-
-	# loop over the frames from the video stream
-	def run(self):
-		print("[INFO] loading face detector model...")
-		print("[INFO] loading face mask detector model...")
-		print("[INFO] starting video stream...")
-		# initialize the video stream and allow the camera sensor to warm up
-		vs = VideoStream(src=self.source).start()
-		time.sleep(2.0)
-		
-		while True:
-			# grab the frame from the threaded video stream and resize it
-			# to have a maximum width of 400 pixels
-			
-			frame = vs.read()
-			frame = imutils.resize(frame, width=400)
-			locs,preds = self.detect_and_predict_mask(frame)
-			# detect faces in the frame and determine if they are wearing a
-			# face mask or not
-			
-
-			# loop over the detected face locations and their corresponding
-			# locations
-			for (box, pred) in zip(locs, preds):
-				# unpack the bounding box and predictions
-				(startX, startY, endX, endY) = box
-				(mask, withoutMask) = pred
-
-				# determine the class label and color we'll use to draw
-				# the bounding box and text
-				label = "Mask" if mask > withoutMask else "No Mask"
-				color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
-
-				# include the probability in the label
-				label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
-
-				# display the label and bounding box rectangle on the output
-				# frame
-				cv2.putText(frame, label, (startX, startY - 10),
-					cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-				cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
-
-			# show the output frame
-			cv2.imshow("Frame", frame)
-			key = cv2.waitKey(1) & 0xFF
-
-			# if the `q` key was pressed, break from the loop
-			if key == ord("q"):
-				break
-		cv2.destroyAllWindows()
-		vs.stop()
-
-def run():
-		# construct the argument parser and parse the arguments
-		ap = argparse.ArgumentParser()
-		ap.add_argument("-f", "--face", type=str, default="face_detector", help="path to face detector model directory")
-		ap.add_argument("-m", "--model", type=str, default="mask_detector.model",help="path to trained face mask detector model")
-		ap.add_argument("-c", "--confidence", type=float, default=0.5, help="minimum probability to filter weak detections")
-		ap.add_argument("-s", "--source", type=int, default=0, help="integer for source camera")
-		args = vars(ap.parse_args())
-		program = MaskDetector(args["face"],args["model"],args["confidence"],args["source"])
-		program.run()
-
-if __name__ == "__main__":
-	run()
-	
-	
